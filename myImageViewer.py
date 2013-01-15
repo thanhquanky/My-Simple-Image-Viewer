8ba48664e6bd2d85ef85c9df35aaf87354794a58
thanhquanky <thanhquanky@gmail.com>
2013-01-15 11:30:37 -0500

    added resize image feature added shrink by scale feature added progress bar

--- myImageViewer.py
+++ myImageViewer.py
@@ -2,9 +2,9 @@
 Author: Thanh Ky Quan
 Date: 05/19/2010
 Filename: myImageViewer.py
-Version: 0.1.2 beta
-'''
+Version: 0.1.3 beta
 
+'''
 """
 Copyright (c) <2010> <Thanh Ky Quan>
 
@@ -18,22 +18,25 @@ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLI
 FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
 WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 """
-
+# What is new:
 '''
-What's new:
-
 Version 0.1.1 beta:
 
-    + add convert to grayscale feature
+	+ added convert to grayscale feature
 
 Version 0.1.2 beta:
 
-    + add convert to negative feature
-    + add horizontal scrollbar and vertical scrollbar if necessary
-    + set image's path on title instead of status bar like version 0.1.1 beta
-    + add menubbar for second frame instead of using buttons
-    + set frame's size dynamically based on screen resolution
-    
+	+ added convert to negative feature
+	+ added horizontal scrollbar and vertical scrollbar if necessary
+	+ set image's path on title instead of status bar like version 0.1.1
+	+ added menubbar for second frame instead of using buttons
+	+ set frame's size dynamically based on screen resolution
+
+Version 0.1.3 beta:
+
+	+ added resize image feature
+	+ added shrink by scale feature
+	+ added progress bar
 '''
 
 import wx
@@ -43,7 +46,9 @@ ID_CREDIT = 110
 ID_CONVERT_TO_GREYSCALE = 111
 ID_CONVERT_TO_NEGATIVE = 112
 ID_RESIZE_IMAGE = 113
-DEF_IMAGE = 'hspt.jpg'
+ID_SHRINK_IMAGE = 114
+ID_WHATISNEW = 115
+#DEF_IMAGE = 'hspt.jpg'
 
 
 class MySimpleImageViewer(wx.App):
@@ -56,12 +61,14 @@ class MySimpleImageViewer(wx.App):
         menu_Help = wx.Menu()
         menu_Help.Append(wx.ID_ABOUT, '&About Simple image viewer', 'Click here to learn more about this application')
         menu_Help.Append(110, '&Credit', 'Creditation')
+        menu_Help.Append(ID_WHATISNEW, 'What is &new', 'What is new')
         menu_Tool = wx.Menu()
         menu_Tool.Append(ID_CONVERT_TO_GREYSCALE, 'Convert to &greyscale', 'Convert image to greyscale')
         menu_Tool.Append(ID_CONVERT_TO_NEGATIVE, 'Convert to &negative', 'Convert to negative')
         menu_Tool.Append(ID_RESIZE_IMAGE, '&Resize image', 'Resize image')
+        menu_Tool.Append(ID_SHRINK_IMAGE, '&Shrink image', 'Shrink image')
         menubar.Append(menu_File, '&File')
-        menubar.Append(menu_Tool, '&Tool')
+        menubar.Append(menu_Tool, '&Tools')
         menubar.Append(menu_Help, '&Help')
 
         return menubar
@@ -69,17 +76,36 @@ class MySimpleImageViewer(wx.App):
     def configuration(self):
         self.clientMaxSize = wx.GetDisplaySize()
         self.clientMaxSize = (self.clientMaxSize[0] - 100, self.clientMaxSize[1] - 200)
+        print 'Client Max Size: ' + str(self.clientMaxSize)
         self.appname = "My Simple Image Viewer"
-        self.version = "0.1.2 Beta"
+        self.version = "0.1.3 Beta"
         self.author = "Thanh Ky Quan"
         self.cursor = wx.StockCursor(wx.CURSOR_ARROW)    
+        self.whatisnew = '''
+        Version 0.1.1 beta:
 
+            + added convert to grayscale feature
+
+        Version 0.1.2 beta:
+
+            + added convert to negative feature
+            + added horizontal scrollbar and vertical scrollbar if necessary
+            + set image's path on title instead of status bar like version 0.1.1
+            + added menubbar for second frame instead of using buttons
+            + set frame's size dynamically based on screen resolution
+
+        Version 0.1.3 beta:
+
+            + added resize image feature
+            + added shrink by scale feature
+            + added progress bar
+        '''
         
     def __init__(self, redirect = False, filename = None):
         wx.App.__init__(self, redirect, filename)
         self.frame = wx.Frame(None, -1, title = 'Simple image viewer', size = (500, 500), pos = (0, 0))
         self.panel = wx.PyScrolledWindow(self.frame, pos = (0,0))
-      
+        self.frame.SetBackgroundColour("#FCFCFE")
         self.configuration()
 
         self.frame.SetMenuBar(self.MakeMenubar())
@@ -90,25 +116,18 @@ class MySimpleImageViewer(wx.App):
         self.frame.Bind(wx.EVT_MENU, self.ConvertToGreyscale, id = ID_CONVERT_TO_GREYSCALE)
         self.frame.Bind(wx.EVT_MENU, self.ConvertToNegative, id = ID_CONVERT_TO_NEGATIVE)
         self.frame.Bind(wx.EVT_MENU, self.ResizeImage, id = ID_RESIZE_IMAGE)
-        
-        self.hBox = wx.BoxSizer()
-        self.vBox = wx.BoxSizer(wx.VERTICAL)
-        self.vBox.Add(self.hBox, proportion = 1, flag = wx.EXPAND, border = 0)
-        self.imagePath = DEF_IMAGE        
-        tmpImg = wx.Image(self.imagePath, wx.BITMAP_TYPE_ANY)
+        self.frame.Bind(wx.EVT_MENU, self.Shrink, id = ID_SHRINK_IMAGE)
+        self.frame.Bind(wx.EVT_MENU, self.WhatIsNew, id = ID_WHATISNEW)
 
-        self.frame.SetClientSize(tmpImg.GetSize())
-        self.imageBox = wx.StaticBitmap(self.panel, wx.ID_ANY, wx.BitmapFromImage(tmpImg))
-        self.hBox.Add(self.imageBox, proportion = 1, flag = wx.EXPAND, border = 0)
 
         # Default parameters
-        self.image = tmpImg
+        self.image = ''
         self.frame2 = ''
         self.status = self.frame.CreateStatusBar()
-        self.savePath = ""
-        self.newImage = ""
-        self.status.SetStatusText(str(tmpImg.GetWidth()) + "x" + str(tmpImg.GetHeight()))
-        self.frame.SetTitle(self.appname + " - " + self.imagePath)
+        self.savePath = ''
+        self.imagePath = ''
+        self.imageBox = ''
+        self.frame.SetTitle(self.appname)
         
         self.panel.Layout()
         self.frame.Show()
@@ -122,6 +141,12 @@ class MySimpleImageViewer(wx.App):
         for teacher in teachers:
             creditMessage += " - " + teacher + "\n"
         msgBox = wx.MessageDialog(None, message= creditMessage, caption = 'Credit', style = wx.OK | wx.STAY_ON_TOP)
+        if (msgBox.ShowModal() == wx.ID_OK):
+            msgBox.Destroy()
+
+    # show what is new
+    def WhatIsNew(self, event):
+        msgBox = wx.MessageDialog(None, message= self.whatisnew, caption = 'What is new', style = wx.OK | wx.STAY_ON_TOP)
         if (msgBox.ShowModal() == wx.ID_OK):
             msgBox.Destroy()
 
@@ -142,14 +167,14 @@ class MySimpleImageViewer(wx.App):
             msgBox.Destroy()
 
     #check image size and max size to add scrollbar if needed
-    def IsEnableScrollbar(self, frame_size):
+    def IsEnableScrollbar(self, image_size):
         scroll = [False, False]
-        
-        if (frame_size[0] > self.clientMaxSize[0]):
+        print 'Image size 2: ' + str(image_size) + '\n' + str(self.clientMaxSize[0])        
+        if (image_size[0] > self.clientMaxSize[0]):
             scroll[0] = True
-        if (frame_size[1] > self.clientMaxSize[1]):
+        if (image_size[1] > self.clientMaxSize[1]):
             scroll[1] = True
-
+        print 'Scroll: ' + str(scroll)
         return scroll
 
     #find possible ratio of scrollbar
@@ -184,13 +209,19 @@ class MySimpleImageViewer(wx.App):
     #load image method    
     def loadImage(self):
         self.image = wx.Image(self.imagePath, wx.BITMAP_TYPE_ANY)
+        self.imageBox = wx.StaticBitmap(self.frame, -1, pos = (0,0))
         self.imageBox.SetBitmap(wx.BitmapFromImage(self.image))
         self.status.SetStatusText(str(self.image.GetWidth()) + "x" + str(self.image.GetHeight()))
         self.frame.SetTitle(self.appname + " - " + self.imagePath)
-        frame_size = self.AdjustFrameSize(self.image.GetSize())
-        scrollbar = self.IsEnableScrollbar(self.image.GetSize())
+        img_size = self.image.GetSize()
+        print 'Image size: ' + str(img_size)
+        frame_size = self.AdjustFrameSize(img_size)
+        print 'Frame size: ' + str(frame_size)
+        scrollbar = self.IsEnableScrollbar(img_size)
+        print 'Scrollbar: ' + str(scrollbar)
         self.panel.EnableScrolling(scrollbar[0], scrollbar[1])
-        ratio = self.GetScrollbarRatio(self.image.GetSize())
+        ratio = self.GetScrollbarRatio(img_size)
+        print 'Ratio : '  + str(ratio)
         self.panel.SetScrollbars(ratio[0], ratio[1], ratio[2], ratio[3])
         self.panel.AdjustScrollbars()
         self.frame.SetClientSize(frame_size)
@@ -224,6 +255,10 @@ class MySimpleImageViewer(wx.App):
         frame.Bind(wx.EVT_MENU, self.OnCredit, id = ID_CREDIT)
         frame.Bind(wx.EVT_MENU, self.ConvertToGreyscale, id = ID_CONVERT_TO_GREYSCALE)
         frame.Bind(wx.EVT_MENU, self.ConvertToNegative, id = ID_CONVERT_TO_NEGATIVE)
+        frame.Bind(wx.EVT_MENU, self.ResizeImage, id = ID_RESIZE_IMAGE)
+        frame.Bind(wx.EVT_MENU, self.Shrink, id = ID_SHRINK_IMAGE)
+        frame.Bind(wx.EVT_MENU, self.WhatIsNew, id = ID_WHATISNEW)
+        
         frame.SetClientSize(frame_size)        
         frame.Show()
 
@@ -245,23 +280,27 @@ class MySimpleImageViewer(wx.App):
         imageBox = wx.StaticBitmap(panel, wx.ID_ANY, wx.BitmapFromImage(img))
         frame_size = self.AdjustFrameSize(img.GetSize())
         
-        scrollbar = self.IsEnableScrollbar(frame_size)
+        scrollbar = self.IsEnableScrollbar(img.GetSize())
         panel.EnableScrolling(scrollbar[0], scrollbar[1])
-        ratio = self.GetScrollbarRatio(frame_size)
+        ratio = self.GetScrollbarRatio(img.GetSize())
         panel.SetScrollbars(ratio[0], ratio[1], ratio[2], ratio[3])
         panel.AdjustScrollbars()
         frame.SetClientSize(frame_size)
         panel.SetScale(0.1, 0.1)
 
         frame.SetMenuBar(self.MakeMenubar())
-        frame.SetMenuBar(self.MakeMenubar())
+        
         frame.Bind(wx.EVT_MENU, self.SaveImage, id = wx.ID_SAVE)
         frame.Bind(wx.EVT_MENU, self.OnOpen, id=wx.ID_OPEN)
         frame.Bind(wx.EVT_MENU, self.OnClickExit, id=wx.ID_EXIT)
         frame.Bind(wx.EVT_MENU, self.OnAbout, id =wx.ID_ABOUT)
         frame.Bind(wx.EVT_MENU, self.OnCredit, id = ID_CREDIT)
         frame.Bind(wx.EVT_MENU, self.ConvertToGreyscale, id = ID_CONVERT_TO_GREYSCALE)
-        frame.Bind(wx.EVT_MENU, self.ConvertToNegative, id = ID_CONVERT_TO_NEGATIVE)        
+        frame.Bind(wx.EVT_MENU, self.ConvertToNegative, id = ID_CONVERT_TO_NEGATIVE)
+        frame.Bind(wx.EVT_MENU, self.ResizeImage, id = ID_RESIZE_IMAGE)
+        frame.Bind(wx.EVT_MENU, self.Shrink, id = ID_SHRINK_IMAGE)
+        frame.Bind(wx.EVT_MENU, self.WhatIsNew, id = ID_WHATISNEW)
+        
         frame.SetClientSize(frame_size)        
         frame.Show()
 
@@ -278,6 +317,8 @@ class MySimpleImageViewer(wx.App):
             getHeight = wx.TextEntryDialog(None, message = "Enter new width (current: " + str(h) + "): ", caption = "Resize Image", defaultValue = str(h))
             if getHeight.ShowModal() == wx.ID_OK:
                 h = getHeight.GetValue()
+        else:
+            frame.Destroy()
         self.newImage = self.image.Scale((int) (w), (int) (h))
         img = self.newImage
         imageBox = wx.StaticBitmap(panel, wx.ID_ANY, wx.BitmapFromImage(img))
@@ -292,6 +333,48 @@ class MySimpleImageViewer(wx.App):
         panel.SetScale(0.1, 0.1)
 
         frame.SetMenuBar(self.MakeMenubar())
+        frame.Bind(wx.EVT_MENU, self.SaveImage, id = wx.ID_SAVE)
+        frame.Bind(wx.EVT_MENU, self.OnOpen, id=wx.ID_OPEN)
+        frame.Bind(wx.EVT_MENU, self.OnClickExit, id=wx.ID_EXIT)
+        frame.Bind(wx.EVT_MENU, self.OnAbout, id =wx.ID_ABOUT)
+        frame.Bind(wx.EVT_MENU, self.OnCredit, id = ID_CREDIT)
+        frame.Bind(wx.EVT_MENU, self.ConvertToGreyscale, id = ID_CONVERT_TO_GREYSCALE)
+        frame.Bind(wx.EVT_MENU, self.ConvertToNegative, id = ID_CONVERT_TO_NEGATIVE)
+        frame.Bind(wx.EVT_MENU, self.ResizeImage, id = ID_RESIZE_IMAGE)
+        frame.Bind(wx.EVT_MENU, self.Shrink, id = ID_SHRINK_IMAGE)
+        
+        frame.Bind(wx.EVT_MENU, self.WhatIsNew, id = ID_WHATISNEW)
+        frame.SetClientSize(frame_size)        
+        frame.Show()
+                    
+    # shrink image
+    def Shrink(self, event):
+        frame = wx.Frame(None, -1, 'Resize Image', pos = (0,0))
+        self.frame2 = frame
+        panel = wx.PyScrolledWindow(frame, style = wx.STAY_ON_TOP)
+        scaleX = 1
+        scaleY = 1
+        getScaleX = wx.TextEntryDialog(None, message = "Enter x factor (current: " + str(1) + "): ", caption = "Shrink Image", defaultValue = str(2))
+        if getScaleX.ShowModal() == wx.ID_OK:
+            scaleX = getScaleX.GetValue()
+            getScaleY = wx.TextEntryDialog(None, message = "Enter y factor (current: " + str(1) + "): ", caption = "Shrink Image", defaultValue = str(2))
+            if getScaleY.ShowModal() == wx.ID_OK:
+                scaleY = getScaleY.GetValue()
+        else:
+            frame.Destroy()
+        self.newImage = self.image.ShrinkBy((float) (scaleX), (float) (scaleY))
+        img = self.newImage
+        imageBox = wx.StaticBitmap(panel, wx.ID_ANY, wx.BitmapFromImage(img))
+        frame_size = self.AdjustFrameSize(img.GetSize())
+        
+        scrollbar = self.IsEnableScrollbar(frame_size)
+        panel.EnableScrolling(scrollbar[0], scrollbar[1])
+        ratio = self.GetScrollbarRatio(frame_size)
+        panel.SetScrollbars(ratio[0], ratio[1], ratio[2], ratio[3])
+        panel.AdjustScrollbars()
+        frame.SetClientSize(frame_size)
+        panel.SetScale(0.1, 0.1)
+
         frame.SetMenuBar(self.MakeMenubar())
         frame.Bind(wx.EVT_MENU, self.SaveImage, id = wx.ID_SAVE)
         frame.Bind(wx.EVT_MENU, self.OnOpen, id=wx.ID_OPEN)
@@ -299,11 +382,12 @@ class MySimpleImageViewer(wx.App):
         frame.Bind(wx.EVT_MENU, self.OnAbout, id =wx.ID_ABOUT)
         frame.Bind(wx.EVT_MENU, self.OnCredit, id = ID_CREDIT)
         frame.Bind(wx.EVT_MENU, self.ConvertToGreyscale, id = ID_CONVERT_TO_GREYSCALE)
-        frame.Bind(wx.EVT_MENU, self.ConvertToNegative, id = ID_CONVERT_TO_NEGATIVE)        
+        frame.Bind(wx.EVT_MENU, self.ConvertToNegative, id = ID_CONVERT_TO_NEGATIVE)
+        frame.Bind(wx.EVT_MENU, self.ResizeImage, id = ID_RESIZE_IMAGE)
+        frame.Bind(wx.EVT_MENU, self.Shrink, id = ID_SHRINK_IMAGE)
+        frame.Bind(wx.EVT_MENU, self.WhatIsNew, id = ID_WHATISNEW)        
         frame.SetClientSize(frame_size)        
         frame.Show()
-                    
-                
         
     #clear frame
     def ClearFrame(self, event):
